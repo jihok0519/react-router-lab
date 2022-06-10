76a51c8a08df27b9ea33c3cdab28b2820c28766f
Enoch Kim <jihok0519@gmail.com>
2022-06-10 14:22:11 -0400

    navigation bar completed

---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <nav className="nav">
+      <Link to="/">
+          <div>iStocks</div>
+          </Link>
+      <Link to="/stocks">
+          <div>Stocks</div>
+          </Link>
+      <Link to="/about">
+          <div>About</div>
+          </Link>
+   </nav>
+  );
+}
+
+export default Nav;
