918d2f129b08ebcaefc478d9940c1f4586919cae
Enoch Kim <jihok0519@gmail.com>
2022-06-10 14:46:11 -0400

    stocks info displays

---
+++ src/pages/StocksInfo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const StockInfo = (props) => {
+  const stock = props.stockData.filter( data => data.symbol === props.match.params.symbol)[0]
+  return (
+    <div>
+      <h3>Name: {stock.name}</h3>
+      <h3>Price: {stock.lastPrice}</h3>
+    </div>
+  )
+}
+
+
+export default StockInfo;
