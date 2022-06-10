5e8934ebb8168d374e62ac29041704c87160c865
Enoch Kim <jihok0519@gmail.com>
2022-06-10 14:21:14 -0400

    have all routes working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import Nav from './components/Nav';
+import About from './pages/About';
+import Home from './pages/Home';
+import Stocks from './pages/Stocks';
 import './App.css';
+import { Route } from 'react-router-dom';
 
-function App() {
+const App = (props) => {
   return (
     <div className="App">
-      
+      <Nav />
+      <Route exact path="/">
+       <Home />
+     </Route>
+     <Route path="/about">
+       <About />
+       </Route>
     </div>
   );
 }
