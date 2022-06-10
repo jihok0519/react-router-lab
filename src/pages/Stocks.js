96c631bc8aefa48947f6d66952894fe1132b13dd
Enoch Kim <jihok0519@gmail.com>
2022-06-10 14:45:45 -0400

    made stocks route accessible, clicking on link will bring you to info

--- src/pages/Stocks.js
+++ src/pages/Stocks.js
@@ -1,5 +1,13 @@
-const Stocks = () => {
+import React from "react";
+import { Link } from 'react-router-dom'
 
+function Stocks(props) {
+  let stockData = props.stockData.map((data, index) => {
+    return <Link key={index} to={`/stocks/${data.symbol}`}>
+        <li >{data.name}</li>
+      </Link>
+  });
+  return <ul>{stockData}</ul>;
 }
 
 export default Stocks;
