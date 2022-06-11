a6381cda8a18cbac85016dcbd67b83612ce2809a
Enoch Kim <jihok0519@gmail.com>
2022-06-11 15:29:05 -0400

    added className to make styling easier

--- src/pages/StocksInfo.js
+++ src/pages/StocksInfo.js
@@ -3,7 +3,7 @@ import React from 'react'
 const StockInfo = (props) => {
   const stock = props.stockData.filter( data => data.symbol === props.match.params.symbol)[0]
   return (
-    <div>
+    <div className="stockInfo">
       <h3>Name: {stock.name}</h3>
       <h3>Price: {stock.lastPrice}</h3>
     </div>
